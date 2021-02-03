5e7b20a530ad702202df023be694ecd042f09ed7
SEVERUS09 <71628612+SEVERUS09@users.noreply.github.com>
2021-02-02 22:42:22 -0500

    Create ds.js

---
+++ Code/ds.js
@@ -0,0 +1,243 @@
+var answerMG = "MEGA";
+var answerZS = "ZIPPYSHARE";
+var answerST = "STAPE";
+
+var des1_1 = "";
+var des1_2 = "";
+var des1_3 = "";
+
+var des2_1 = "";
+var des2_2 = "";
+var des2_3 = "";
+
+var des3_1 = "";
+var des3_2 = "";
+var des3_3 = "";
+
+var des4_1 = "";
+var des4_2 = "";
+var des4_3 = "";
+
+var des5_1 = "";
+var des5_2 = "";
+var des5_3 = "";
+
+var des6_1 = "";
+var des6_2 = "";
+var des6_3 = "";
+
+var des7_1 = "";
+var des7_2 = "";
+var des7_3 = "";
+
+var des8_1 = "";
+var des8_2 = "";
+var des8_3 = "";
+
+var des9_1 = "";
+var des9_2 = "";
+var des9_3 = "";
+
+var des10_1 = "";
+var des10_2 = "";
+var des10_3 = "";
+
+var des11_1 = "";
+var des11_2 = "";
+var des11_3 = "";
+
+var des12_1 = "";
+var des12_2 = "";
+var des12_3 = "";
+
+var des13_1 = "";
+var des13_2 = "";
+var des13_3 = "";
+
+var des14_1 = "";
+var des14_2 = "";
+var des14_3 = "";
+
+var des15_1 = "";
+var des15_2 = "";
+var des15_3 = "";
+
+var des16_1 = "";
+var des16_2 = "";
+var des16_3 = "";
+
+var des17_1 = "";
+var des17_2 = "";
+var des17_3 = "";
+
+var des18_1 = "";
+var des18_2 = "";
+var des18_3 = "";
+
+var des19_1 = "";
+var des19_2 = "";
+var des19_3 = "";
+
+var des20_1 = "";
+var des20_2 = "";
+var des20_3 = "";
+
+var des21_1 = "";
+var des21_2 = "";
+var des21_3 = "";
+
+var des22_1 = "";
+var des22_2 = "";
+var des22_3 = "";
+
+var des23_1 = "";
+var des23_2 = "";
+var des23_3 = "";
+
+var des24_1 = "";
+var des24_2 = "";
+var des24_3 = "";
+
+var des25_1 = "";
+var des25_2 = "";
+var des25_3 = "";
+
+var des26_1 = "";
+var des26_2 = "";
+var des26_3 = "";
+
+var des27_1 = "";
+var des27_2 = "";
+var des27_3 = "";
+
+var des28_1 = "";
+var des28_2 = "";
+var des28_3 = "";
+
+var des29_1 = "";
+var des29_2 = "";
+var des29_3 = "";
+
+var des30_1 = "";
+var des30_2 = "";
+var des30_3 = "";
+
+var des31_1 = "";
+var des31_2 = "";
+var des31_3 = "";
+
+var des32_1 = "";
+var des32_2 = "";
+var des32_3 = "";
+
+var des33_1 = "";
+var des33_2 = "";
+var des33_3 = "";
+
+var des34_1 = "";
+var des34_2 = "";
+var des34_3 = "";
+
+var des35_1 = "";
+var des35_2 = "";
+var des35_3 = "";
+
+var des36_1 = "";
+var des36_2 = "";
+var des36_3 = "";
+
+var des37_1 = "";
+var des37_2 = "";
+var des37_3 = "";
+
+var des38_1 = "";
+var des38_2 = "";
+var des38_3 = "";
+
+var des39_1 = "";
+var des39_2 = "";
+var des39_3 = "";
+
+var des40_1 = "";
+var des40_2 = "";
+var des40_3 = "";
+
+var des41_1 = "";
+var des41_2 = "";
+var des41_3 = "";
+
+var des42_1 = "";
+var des42_2 = "";
+var des42_3 = "";
+
+var des43_1 = "";
+var des43_2 = "";
+var des43_3 = "";
+
+var des44_1 = "";
+var des44_2 = "";
+var des44_3 = "";
+
+var des45_1 = "";
+var des45_2 = "";
+var des45_3 = "";
+
+var des46_1 = "";
+var des46_2 = "";
+var des46_3 = "";
+
+var des47_1 = "";
+var des47_2 = "";
+var des47_3 = "";
+
+var des48_1 = "";
+var des48_2 = "";
+var des48_3 = "";
+
+var des49_1 = "";
+var des49_2 = "";
+var des49_3 = "";
+
+var des50_1 = "";
+var des50_2 = "";
+var des50_3 = "";
+
+var des51_1 = "";
+var des51_2 = "";
+var des51_3 = "";
+
+var des52_1 = "";
+var des52_2 = "";
+var des52_3 = "";
+
+var des53_1 = "";
+var des53_2 = "";
+var des53_3 = "";
+
+var des54_1 = "";
+var des54_2 = "";
+var des54_3 = "";
+
+var des55_1 = "";
+var des55_2 = "";
+var des55_3 = "";
+
+var des56_1 = "";
+var des56_2 = "";
+var des56_3 = "";
+
+var des57_1 = "";
+var des57_2 = "";
+var des57_3 = "";
+
+var des58_1 = "";
+var des58_2 = "";
+var des58_3 = "";
+
+var des59_1 = "";
+var des59_2 = "";
+var des59_3 = "";
+
+var des60_1 = "";
+var des60_2 = "";
+var des60_3 = "";
