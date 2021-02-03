b483bcc0410be989cc2b41720ce702fc4630940b
SEVERUS09 <71628612+SEVERUS09@users.noreply.github.com>
2021-02-03 00:46:57 -0500

    Create 3324.js

---
+++ episodios/3324.js
@@ -0,0 +1,118 @@
+var EPR1_1 = "https://severus09.github.io/AnimeCFD/3324-1.html";
+var EPR1_2 = "https://severus09.github.io/AnimeCFD/v/3324-1v2.html";
+var EPR1_3 = "https://embedsito.com/v/8pyd0f8pq3ywdnq";
+var EPR1_4 = "https://streamtape.com/e/lAyJqJPl6LT7Mar/";
+var EPR1_5 = "https://www.yourupload.com/embed/8CDq4y8F1I6W";
+var EPR1_6 = "https://mega.nz/embed#!FEhRFAYK!3TKaOdOdlZ6OE9SsnIG-Pxr1E3nrQ_d73LUMetHLjec";
+
+var EPR2_1 = "https://severus09.github.io/AnimeCFD/v/3324-2.html";
+var EPR2_2 = "https://severus09.github.io/AnimeCFD/v/3324-2v2.html";
+var EPR2_3 = "https://embedsito.com/v/y26kwhej6e0g8-3";
+var EPR2_4 = "https://streamtape.com/e/p7VQ31lbYvFXL7/";
+var EPR2_5 = "https://www.yourupload.com/embed/QX7qm07snHWs";
+var EPR2_6 = "https://mega.nz/embed#!pEwnyQRB!cq36dZHmmpFu2zPqK1OrtBLOGq-Ku4-tqPRNxfEAqs0";
+
+var EPR3_1 = "https://severus09.github.io/AnimeCFD/v/3324-3.html";
+var EPR3_2 = "https://severus09.github.io/AnimeCFD/v/3324-3v2.html";
+var EPR3_3 = "https://embedsito.com/v/x431gb51rg-4m2n";
+var EPR3_4 = "https://streamtape.com/e/J9KgXvx1L4Cjx36/";
+var EPR3_5 = "https://www.yourupload.com/embed/CCmtwVI78NBj";
+var EPR3_6 = "https://mega.nz/embed#!ZMhnhISJ!mTkh-lxIU-gTaDjglXS03kZp3br5Pcd4fndKwQsJ2_8";
+
+var EPR4_1 = "https://severus09.github.io/AnimeCFD/v/3324-4.html";
+var EPR4_2 = "https://severus09.github.io/AnimeCFD/v/3324-4v2.html";
+var EPR4_3 = "https://embedsito.com/v/y26kwhejqz572r1";
+var EPR4_4 = "https://streamtape.com/e/KL0xMeglZgh0k7k/";
+var EPR4_5 = "https://www.yourupload.com/embed/1mj4jpfKlos3";
+var EPR4_6 = "https://mega.nz/embed#!vhwBkDTB!yOqSS85sjZlPZgcjc4IPB4NyMW2Dql9V6bHFDj8h6Lk";
+
+var EPR5_1 = "https://severus09.github.io/AnimeCFD/v/3324-5.html";
+var EPR5_2 = "https://severus09.github.io/AnimeCFD/v/3324-5v2.html";
+var EPR5_3 = "https://embedsito.com/v/x431gb51k7x5zy7";
+var EPR5_4 = "https://streamtape.com/e/ao2L3JR4jLsxGvg/";
+var EPR5_5 = "https://www.yourupload.com/embed/07mnfQRIHRD1";
+var EPR5_6 = "https://mega.nz/embed#!4A41FIbT!CNnvHSIt0plAmnTb7Ao_C3pyBVg0yZ90OSdjANVy3q8";
+
+var EPR6_1 = "";
+var EPR6_2 = "";
+var EPR6_3 = "";
+var EPR6_4 = "";
+var EPR6_5 = "";
+var EPR6_6 = "";
+
+var EPR7_1 = "";
+var EPR7_2 = "";
+var EPR7_3 = "";
+var EPR7_4 = "";
+var EPR7_5 = "";
+var EPR7_6 = "";
+
+var EPR8_1 = "";
+var EPR8_2 = "";
+var EPR8_3 = "";
+var EPR8_4 = "";
+var EPR8_5 = "";
+var EPR8_6 = "";
+
+var EPR9_1 = "";
+var EPR9_2 = "";
+var EPR9_3 = "";
+var EPR9_4 = "";
+var EPR9_5 = "";
+var EPR9_6 = "";
+
+var EPR10_1 = "";
+var EPR10_2 = "";
+var EPR10_3 = "";
+var EPR10_4 = "";
+var EPR10_5 = "";
+var EPR10_6 = "";
+
+var EPR11_1 = "";
+var EPR11_2 = "";
+var EPR11_3 = "";
+var EPR11_4 = "";
+var EPR11_5 = "";
+var EPR11_6 = "";
+
+var EPR12_1 = "";
+var EPR12_2 = "";
+var EPR12_3 = "";
+var EPR12_4 = "";
+var EPR12_5 = "";
+var EPR12_6 = "";
+
+var EPR13_1 = "";
+var EPR13_2 = "";
+var EPR13_3 = "";
+var EPR13_4 = "";
+var EPR13_5 = "";
+var EPR13_6 = "";
+
+var EPR14_1 = "";
+var EPR14_2 = "";
+var EPR14_3 = "";
+var EPR14_4 = "";
+var EPR14_5 = "";
+var EPR14_6 = "";
+
+var EPR15_1 = "";
+var EPR15_2 = "";
+var EPR15_3 = "";
+var EPR15_4 = "";
+var EPR15_5 = "";
+var EPR15_6 = "";
+
+var EPR16_1 = "";
+var EPR16_2 = "";
+var EPR16_3 = "";
+var EPR16_4 = "";
+var EPR16_5 = "";
+var EPR16_6 = "";
+
+var EPR17_1 = "";
+var EPR17_2 = "";
+var EPR17_3 = "";
+var EPR17_4 = "";
+var EPR17_5 = "";
+var EPR17_6 = "";
