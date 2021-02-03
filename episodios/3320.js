4bf998adf19e86a97cc7d408803d80b62ca0af4c
SEVERUS09 <71628612+SEVERUS09@users.noreply.github.com>
2021-02-02 22:04:06 -0500

    Create 3320.js

---
+++ episodios/3320.js
@@ -0,0 +1,59 @@
+var EPR1_1 = "https://severus09.github.io/AnimeCFD/3320-1.html";
+var EPR1_2 = "https://repro-cfd.blogspot.com/p/3320-1v2.html";
+var EPR1_3 = "https://v2.zplayer.live/embed/xy19di8iki2d";
+var EPR1_4 = "https://streamtape.com/e/Bzyo8kD2X8Iyqo3/";
+
+var EPR2_1 = "https://repro-cfd.blogspot.com/p/3320-2.html";
+var EPR2_2 = "https://repro-cfd.blogspot.com/p/3320-2v2.html";
+var EPR2_3 = "https://v2.zplayer.live/embed/kdvz5x5x168n";
+var EPR2_4 = "https://streamtape.com/e/lwbwQkdD8ZCowl/";
+
+var EPR3_1 = "https://repro-cfd.blogspot.com/p/3320-3.html";
+var EPR3_2 = "https://repro-cfd.blogspot.com/p/3320-3v2.html";
+var EPR3_3 = "https://embedsito.com/v/py13gum775j1343";
+var EPR3_4 = "https://streamtape.com/e/MP2VKZ3kwkFmb6a/";
+
+var EPR4_1 = "https://repro-cfd.blogspot.com/p/3320-4.html";
+var EPR4_2 = "https://repro-cfd.blogspot.com/p/3320-4v2.html";
+var EPR4_3 = "https://embedsito.com/v/w3wpgan2z3lnn-z";
+var EPR4_4 = "https://streamtape.com/e/7m3K1drxAytAjb3/";
+
+var EPR5_1 = "https://repro-cfd.blogspot.com/p/3320-5.html";
+var EPR5_2 = "https://repro-cfd.blogspot.com/p/3320-5v2.html";
+var EPR5_3 = "https://embedsito.com/v/y26kwhej3w84m6z";
+var EPR5_4 = "https://streamtape.com/e/b7mrKBDJG6FP83g/";
+
+var EPR6_1 = "https://repro-cfd.blogspot.com/p/3320-6.html";
+var EPR6_2 = "https://repro-cfd.blogspot.com/p/3320-6v2.html";
+var EPR6_3 = "https://embedsito.com/v/dk125hx8--j3rkx";
+var EPR6_4 = "https://streamtape.com/e/labWKYwGo4C746W/";
+
+var EPR7_1 = "https://repro-cfd.blogspot.com/p/3320-7.html";
+var EPR7_2 = "https://repro-cfd.blogspot.com/p/3320-7v2.html";
+var EPR7_3 = "https://embedsito.com/v/ryq6gueyn6l5qlz";
+var EPR7_4 = "https://streamtape.com/e/Y7Lkr404x1Tv90V/";
+
+var EPR8_1 = "https://repro-cfd.blogspot.com/p/3320-8.html";
+var EPR8_2 = "https://repro-cfd.blogspot.com/p/3320-8v2.html";
+var EPR8_3 = "https://embedsito.com/v/w3wpgan2kj-470x";
+var EPR8_4 = "https://streamtape.com/e/3DRzP9Y9xXUD8v/";
+
+var EPR9_1 = "https://repro-cfd.blogspot.com/p/3320-9.html";
+var EPR9_2 = "https://repro-cfd.blogspot.com/p/3320-9v2.html";
+var EPR9_3 = "https://embedsito.com/v/nyd8gu23dkql3xl";
+var EPR9_4 = "https://streamtape.com/e/DlblO7Z9b3tk9GQ/";
+
+var EPR10_1 = "https://repro-cfd.blogspot.com/p/3320-10.html";
+var EPR10_2 = "https://repro-cfd.blogspot.com/p/3320-10v2.html";
+var EPR10_3 = "https://embedsito.com/v/x431gb52dwmdm4e";
+var EPR10_4 = "https://streamtape.com/e/AopebYPVDruvyp/";
+
+var EPR11_1 = "https://repro-cfd.blogspot.com/p/3320-10_30.html";
+var EPR11_2 = "https://repro-cfd.blogspot.com/p/3320-11v2.html";
+var EPR11_3 = "https://embedsito.com/v/-z85qtp6w7y824-";
+var EPR11_4 = "https://streamtape.com/e/ow0YZbPjyACWdR/";
+
+var EPR12_1 = "https://severus09.github.io/AnimeCFD/3320-12.html";
+var EPR12_2 = "https://repro-cfd.blogspot.com/p/3320-12.html";
+var EPR12_3 = "https://embedsito.com/v/47134cz8wn0y3nr";
+var EPR12_4 = "https://streamtape.com/e/QAPmajpAZdT0j9r/";
