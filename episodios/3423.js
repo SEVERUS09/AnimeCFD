d4e886e980001a75209e24f132bd512593bf7ad7
SEVERUS09 <71628612+SEVERUS09@users.noreply.github.com>
2021-02-04 12:28:36 -0500

    Create 3423.js

---
+++ episodios/3423.js
@@ -0,0 +1,27 @@
+var EPR1_1 = "https://streamium.xyz/gocdn.html#aUdVL1ArN0dOOEt1OFFPYzRlTERrcjlscWdPZDlyMzdnZnpOUzVCcTlCaDUxckFpSGxtOGNDVVRpdmxKWDVML1hvdUdlbGJEM1RBelZ2WlRFNDNiem90cWt4Ymd3QjFWRHdHeGZuVFF3UzU4THphakZxb0xLWTdSYTdGNTQwVG4=";
+var EPR1_2 = "https://streamtape.com/e/OPJbMZKmVOfZJQg/";
+var EPR1_3 = "https://embedsito.com/v/gqlmyt-z7gz3ngd";
+var EPR1_4 = "https://hqq.tv/player/embed_player.php?vid=Zk0veUJYcmg5QXhwd2xkUnRTV08xUT09";
+var EPR1_5 = "https://www.yourupload.com/embed/gie8vITSxygO";
+var EPR1_6 = "https://mega.nz/embed#!mRFgwRzb!ROv8VjJmlBImCPhe51D23L9V4DIXD9hG3Juo91jFnK4";
+
+var EPR2_1 = "https://streamium.xyz/gocdn.html#eWVjTi8zS0k0QnBvRytVY0pjNlJvMVR2TUhGY2V3aTZ5YVZ1UnlKMmc5L1ZqNXJFTHVNSmpJbjlEZHdrTTNOOEZST1U1YTNKajE0V0JSeVRGcFJzUjBMK0MrbWdlRUNpVWdoc2lWZ2ZkZ1ZCN2puUDkzb3dhNEJjZkVoSHhENnM=";
+var EPR2_2 = "https://streamtape.com/e/pDalBKwW7RCrYkV/";
+var EPR2_3 = "https://embedsito.com/v/x431gb5q0w5m8km";
+var EPR2_4 = "https://hqq.tv/player/embed_player.php?vid=SjRFWlM4WXFIeXFDVUVqZkQxZ2lLdz09";
+var EPR2_5 = "https://www.yourupload.com/embed/S865p88MLp4k";
+var EPR2_6 = "https://mega.nz/embed#!KFkDAajS!3X9psIgeI4hX7jo0OsCAjCccqsURkpfwObQbyWV7J8Y";
+
+var EPR3_1 = "https://streamium.xyz/gocdn.html#SW9UR0g0RnhGZTFFN29yZ2pVR1hwR2NmNnJlYkxGVjhOY3ZIMVUyWDVqZTIrMjBkSnZvR1hjdmpkeXhGRjdVZFhWQ3U0WEs5T3dwdERuOFRaWGJ2SGU4Y2lLNUg2a29YWWptVU9hZmJ5ajI1QkYxd0xTRUtteGFMZGFucWVhZjM=";
+var EPR3_2 = "https://streamtape.com/e/e3x9KVve19foPV/";
+var EPR3_3 = "https://embedsito.com/v/ln3ygsneq1nplml";
+var EPR3_4 = "https://hqq.tv/player/embed_player.php?vid=dzFibmJxMk9oTitPcTdDTHB6d0ZVQT09";
+var EPR3_5 = "https://www.yourupload.com/embed/K8xroLodYBRM";
+var EPR3_6 = "https://mega.nz/embed#!zclB2AqY!txbG4Ygjzn7ZkxmQfXsUSx9hYbx_I0qBjvq5wbomxOQ";
+
+var EPR4_1 = "https://streamium.xyz/gocdn.html#ZUhUQytRQS9zMnIreERiQkJVcEZXNXVycHpjZDdhUUh3UDcrSjlRc0pHdXFHZVh3blg1bjJ4U1U5QzlNYjh3Nkh0QXBMZ2oxalBucDdnemU2SHpRZ3I1WVdUY2lERVNRWTR4NmU0UnJxbWVmY3VOT2pIK0F3aTJ3OVB3UTdXOVA=";
+var EPR4_2 = "https://streamtape.com/e/7zoDVj3bP7CApXw/";
+var EPR4_3 = "https://embedsito.com/v/8pyd0f8yq78jnxr";
+var EPR4_4 = "https://hqq.tv/player/embed_player.php?vid=VXp1TU5XbE1VTVJxREN6WUVqUlBRdz09";
+var EPR4_5 = "https://www.yourupload.com/embed/5VvolcTlol8d";
+var EPR4_6 = "https://mega.nz/embed#!oR8GDRhZ!ob3agmtqxLuf3Chj7OcAv1bVoxEG1HQwI3XsSjWnDdI";
