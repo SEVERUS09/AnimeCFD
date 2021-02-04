d99fcdb21ada0da12d403df7ecfa8d9f9e9f1d36
SEVERUS09 <71628612+SEVERUS09@users.noreply.github.com>
2021-02-03 21:38:13 -0500

    Create desc-3324.js

---
+++ ds/desc-3324.js
@@ -0,0 +1,239 @@
+var des1_1 = "https://mega.nz/#!FEhRFAYK!3TKaOdOdlZ6OE9SsnIG-Pxr1E3nrQ_d73LUMetHLjec";
+var des1_2 = "https://www106.zippyshare.com/v/6AKP3idb/file.html";
+var des1_3 = "https://streamtape.com/v/lAyJqJPl6LT7Mar/";
+
+var des2_1 = "https://mega.nz/#!pEwnyQRB!cq36dZHmmpFu2zPqK1OrtBLOGq-Ku4-tqPRNxfEAqs0";
+var des2_2 = "https://www2.zippyshare.com/v/mEOyIPJU/file.html";
+var des2_3 = "https://streamtape.com/v/p7VQ31lbYvFXL7/";
+
+var des3_1 = "https://mega.nz/#!ZMhnhISJ!mTkh-lxIU-gTaDjglXS03kZp3br5Pcd4fndKwQsJ2_8";
+var des3_2 = "https://www48.zippyshare.com/v/1TVojOx1/file.html";
+var des3_3 = "https://streamtape.com/v/J9KgXvx1L4Cjx36/";
+
+var des4_1 = "https://mega.nz/#!vhwBkDTB!yOqSS85sjZlPZgcjc4IPB4NyMW2Dql9V6bHFDj8h6Lk";
+var des4_2 = "https://www106.zippyshare.com/v/xFizRjIA/file.html";
+var des4_3 = "https://streamtape.com/v/KL0xMeglZgh0k7k/";
+
+var des5_1 = "https://mega.nz/#!4A41FIbT!CNnvHSIt0plAmnTb7Ao_C3pyBVg0yZ90OSdjANVy3q8";
+var des5_2 = "https://www77.zippyshare.com/v/R3h3dA5Q/file.html";
+var des5_3 = "https://streamtape.com/v/ao2L3JR4jLsxGvg/";
+
+var des6_1 = "https://mega.nz/#!odoV0YAZ!82jvfqKdxfKJsvWeC1r3N0mnZYqKiqx5eftBORgq7WI";
+var des6_2 = "https://www78.zippyshare.com/v/YYQTc54W/file.html";
+var des6_3 = "https://streamtape.com/v/kprqQVXm8ktOgyB/";
+
+var des7_1 = "https://mega.nz/#!EIgjkAyR!P1Cmf3fIgeungLkattY45311PSLNi9BHVkoARGLwDVg";
+var des7_2 = "https://www99.zippyshare.com/v/qLJLjirQ/file.html";
+var des7_3 = "https://streamtape.com/v/e4Lbywd0Ddsky9/";
+
+var des8_1 = "https://mega.nz/#!L4h3hAab!nCqfRcHQBB8YcNQ-UEmNwvc96apb_25Tty8J9X6EyN8";
+var des8_2 = "https://www29.zippyshare.com/v/oHgOeAmd/file.html";
+var des8_3 = "https://streamtape.com/v/Kwvp6AMlQQFaX3/";
+
+var des9_1 = "https://mega.nz/#!JMoFFCKR!PDMXIJdNWZix4Tu2nyVPv2dBPx3OW96qdOr62cuSFpM";
+var des9_2 = "https://www45.zippyshare.com/v/qAMkMSJN/file.html";
+var des9_3 = "https://streamtape.com/v/vPVxl6dMlLtBRe/";
+
+var des10_1 = "https://mega.nz/#!Rc4DmQRI!SMXUIu8aAcDsoVeY9Tpul3NnIIAiDKAOOUjqq7zCX4E";
+var des10_2 = "https://www2.zippyshare.com/v/pX8nGVs1/file.html";
+var des10_3 = "https://streamtape.com/v/bpbkLZ6dP3SQjB/";
+
+var des11_1 = "https://mega.nz/#!oFhjWAYS!I-Xdo0BUiN5CmqT6NOIbakZUY_IgYj7u03A282Jk3V8";
+var des11_2 = "https://www73.zippyshare.com/v/m4fxiLec/file.html";
+var des11_3 = "https://streamtape.com/v/mDDKGw2Z4dIb4r2/";
+
+var des12_1 = "https://mega.nz/#!YB5FmYaS!ZqH-gXWapujG3e5T_OTSotj56tU9P9LzWfvKgCU0xe4";
+var des12_2 = "https://www114.zippyshare.com/v/iW1s8E1n/file.html";
+var des12_3 = "https://streamtape.com/v/xoQ1AoXQWkfkaZB/";
+
+var des13_1 = "https://mega.nz/#!EYoFCIYR!J5QFCRnKePHdlLIbnxAmb3wRuLwscNNWvxXQkCDSfpw";
+var des13_2 = "https://www14.zippyshare.com/v/nj4nYWcS/file.html";
+var des13_3 = "https://streamtape.com/v/qaoDlOapp9FzzeD/";
+
+var des14_1 = "https://mega.nz/#!gJ5hmCZD!_z_EMr63VjyEMuAX1zO1l5RxXUAM4g6-AzXpEY3Sgok";
+var des14_2 = "https://www98.zippyshare.com/v/0k2WlwHt/file.html";
+var des14_3 = "https://streamtape.com/v/4mXg8DYBQ2cd7a/";
+
+var des15_1 = "https://mega.nz/#!3ZlQ2CLI!TXsCZ6DiQAIwfcC_Z81oHlQKVFLzrzORrPoDPhgzszw";
+var des15_2 = "https://www65.zippyshare.com/v/47uw9SqC/file.html";
+var des15_3 = "https://streamtape.com/v/QdQBqz2pxYu01k2/";
+
+var des16_1 = "https://mega.nz/#!bY1izJJA!JTS2QecaVPzSEyvfx3_HGxEukmZOebGDxx7feG46SbE";
+var des16_2 = "https://www80.zippyshare.com/v/Ugsl2y6p/file.html";
+var des16_3 = "https://streamtape.com/v/xvGO2VpMX6clMa/";
+
+var des17_1 = "https://mega.nz/#!PEVWyThD!VXMKZBo8FHi27_4mGpkPb7XU7yPW53aneDecyAAUnaU";
+var des17_2 = "https://www42.zippyshare.com/v/HIv44DdN/file.html";
+var des17_3 = "https://streamtape.com/v/W34oOo0Appsb9Am/";
+
+var des18_1 = "https://mega.nz/#!oNkxGIrA!o0td996Q3r9TIJfDRwOQABKtWIIRzt1sgp1QtmmT5xA";
+var des18_2 = "https://www39.zippyshare.com/v/loQLadUC/file.html";
+var des18_3 = "https://streamtape.com/v/7dMY6dm1P9uoXL/";
+
+var des19_1 = "";
+var des19_2 = "";
+var des19_3 = "";
+
+var des20_1 = "";
+var des20_2 = "";
+var des20_3 = "";
+
+var des21_1 = "";
+var des21_2 = "";
+var des21_3 = "";
+
+var des22_1 = "";
+var des22_2 = "";
+var des22_3 = "";
+
+var des23_1 = "";
+var des23_2 = "";
+var des23_3 = "";
+
+var des24_1 = "";
+var des24_2 = "";
+var des24_3 = "";
+
+var des25_1 = "";
+var des25_2 = "";
+var des25_3 = "";
+
+var des26_1 = "";
+var des26_2 = "";
+var des26_3 = "";
+
+var des27_1 = "";
+var des27_2 = "";
+var des27_3 = "";
+
+var des28_1 = "";
+var des28_2 = "";
+var des28_3 = "";
+
+var des29_1 = "";
+var des29_2 = "";
+var des29_3 = "";
+
+var des30_1 = "";
+var des30_2 = "";
+var des30_3 = "";
+
+var des31_1 = "";
+var des31_2 = "";
+var des31_3 = "";
+
+var des32_1 = "";
+var des32_2 = "";
+var des32_3 = "";
+
+var des33_1 = "";
+var des33_2 = "";
+var des33_3 = "";
+
+var des34_1 = "";
+var des34_2 = "";
+var des34_3 = "";
+
+var des35_1 = "";
+var des35_2 = "";
+var des35_3 = "";
+
+var des36_1 = "";
+var des36_2 = "";
+var des36_3 = "";
+
+var des37_1 = "";
+var des37_2 = "";
+var des37_3 = "";
+
+var des38_1 = "";
+var des38_2 = "";
+var des38_3 = "";
+
+var des39_1 = "";
+var des39_2 = "";
+var des39_3 = "";
+
+var des40_1 = "";
+var des40_2 = "";
+var des40_3 = "";
+
+var des41_1 = "";
+var des41_2 = "";
+var des41_3 = "";
+
+var des42_1 = "";
+var des42_2 = "";
+var des42_3 = "";
+
+var des43_1 = "";
+var des43_2 = "";
+var des43_3 = "";
+
+var des44_1 = "";
+var des44_2 = "";
+var des44_3 = "";
+
+var des45_1 = "";
+var des45_2 = "";
+var des45_3 = "";
+
+var des46_1 = "";
+var des46_2 = "";
+var des46_3 = "";
+
+var des47_1 = "";
+var des47_2 = "";
+var des47_3 = "";
+
+var des48_1 = "";
+var des48_2 = "";
+var des48_3 = "";
+
+var des49_1 = "";
+var des49_2 = "";
+var des49_3 = "";
+
+var des50_1 = "";
+var des50_2 = "";
+var des50_3 = "";
+
+var des51_1 = "";
+var des51_2 = "";
+var des51_3 = "";
+
+var des52_1 = "";
+var des52_2 = "";
+var des52_3 = "";
+
+var des53_1 = "";
+var des53_2 = "";
+var des53_3 = "";
+
+var des54_1 = "";
+var des54_2 = "";
+var des54_3 = "";
+
+var des55_1 = "";
+var des55_2 = "";
+var des55_3 = "";
+
+var des56_1 = "";
+var des56_2 = "";
+var des56_3 = "";
+
+var des57_1 = "";
+var des57_2 = "";
+var des57_3 = "";
+
+var des58_1 = "";
+var des58_2 = "";
+var des58_3 = "";
+
+var des59_1 = "";
+var des59_2 = "";
+var des59_3 = "";
+
+var des60_1 = "";
+var des60_2 = "";
+var des60_3 = "";
