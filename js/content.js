79e72879f07bdaf245254a2c0703ce81cd5353e8
SEVERUS09 <71628612+SEVERUS09@users.noreply.github.com>
2021-02-21 16:36:33 -0500

    Create content.js

---
+++ js/content.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+    
+    $('.episode-page__servers-list a:first').parent().addClass('is-active').show();
+    var firstTabNumber = $('.episode-page__servers-list a:first').attr('href').substring(4);
+
+    $('#video_player').html(tabsArray[firstTabNumber]);
+
+    $('.episode-page__servers-list a').on('click', function(e) {
+        e.preventDefault();
+
+        $('.episode-page__servers-list li').removeClass('is-active');
+        $(this).parent().addClass('is-active');
+
+        var activeTabNumber = $(this).attr('href').substring(4);
+        $('#video_player').html(tabsArray[activeTabNumber]);
+        console.log("activetab val: " + activeTabNumber);
+    });
+
+    $("#showModal").click(function() {
+        $(".modal").addClass("is-active");
+    });
+
+    $(".js-download-close").click(function() {
+        $(".modal").removeClass("is-active");
+    });
+});
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+function myFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+  }
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("activea");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+// botones de opciones activo
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
